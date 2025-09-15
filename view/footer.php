7d98fa893f60db074e2f5c54609318cba1e8c290
riigreyr <fahriarif97@gmail.com>
2025-09-15 09:36:45 +0700

    update

--- view/footer.php
+++ view/footer.php
@@ -1,12 +1,14 @@
 <footer class="mt-5 py-4 bg-blue-dark text-white text-center" style="padding: 1.5rem 0; font-size: 0.9rem;">
   <div class="container">
-    <strong>BelanjaYuk</strong><br />
-    <small>©<?= date('Y') ?> dikembangkan oleh Fakhri</small><br />
-    <div class="mt-2">
+    <div class="row">
+    <div class="col-6"> <strong>BelanjaYuk</strong><br />
+    <small>©<?= date('Y') ?> dikembangkan oleh Fakhri</small><br /></div>
+    <div class="col-6"><div class="">
       <strong>Kontak Kami</strong><br />
       Dinas Komunikasi, Informatika dan Persandian Aceh<br />
       Jl. Jalan. 10<br />
       Banda Aceh
     </div>
+  </div>
   </div>
 </footer>
